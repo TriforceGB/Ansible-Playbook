dd44cb542c76f37b2bde3a783b8ff0caeaa86f0f
Zach Schultz <Zach.Schultz.2.0.1@gmai.com>
2025-01-10 16:57:29 -0500

    created Scripts to start

---
+++ Scripts/Pre-Ansible.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+# && "$ID" != "ubuntu"  add Later
+# Check if your Using a Supported OS
+source /etc/os-release
+if [[ "$ID" != "arch" ]]; then
+    echo "Unfortunately Your OS is not supported with this Ansiable Playbook"
+    exit 1
+fi
+# Prompt the user to enter wanted Setup
+echo -e "What Setup Do You Want?
+        (1) Workstation
+        (2) Server
+        (3) Base Install"
+read Setup
+
+ansible-pull -U https://github.com/TriforceGB/Anisble_Setup.git -Ke "Setup=$Setup"
+#bash -e "$(curl -fsSL https://raw.githubusercontent.com/TriforceGB/Anisble_Setup/main/Scripts/Pre-Ansible.sh)"
+
+
+
+
+
