d2b3174ce563cf747d96884932a4636171c29ff2
Zach Schultz <Zach.Schultz.2.0.1@gmai.com>
2025-01-10 21:02:04 -0500

    forgot to remove old if statments

--- Scripts/Pre-Ansible.sh
+++ Scripts/Pre-Ansible.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-#bash -e "$(curl -fsSL https://raw.githubusercontent.com/TriforceGB/Anisble_Setup/main/Scripts/Pre-Ansible.sh)"
+#bash -c "$(curl -fsSL https://raw.githubusercontent.com/TriforceGB/Anisble_Setup/main/Scripts/Pre-Ansible.sh)"
 # && "$ID" != "ubuntu"  add Later
+
 # Check if your Using a Supported OS
 source /etc/os-release
-if [[ "$ID" != "arch" ]]; then
-    echo "Unfortunately Your OS is not supported with this Ansiable Playbook"
-    exit 1
-fi
-if [[ "$ID" == "arch" ]]; then
-    echo "Unfortunately Your OS is not supported with this Ansiable Playbook"
-    exit 1
-fi
-
 if [ "$ID" == "arch" ]; then
     sudo pacman -S ansible
 elif [ "$ID" == "ubuntu" ]; then
